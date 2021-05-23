5c8f12e7f5601c937979e186f8048a78766b7ac3
aisyahdamia <nikaisyah.damia8@gmail.com>
2021-05-24 01:34:48 +0800

    first commit

---
+++ app/Models/Cat.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
+
+class Cat extends Model
+{
+    use HasFactory;
+    protected $primaryKey = 'cat_id';
+    protected $fillable = ['name','date_of_birth'];
+    public $timestamps = false;
+
+    public function setDateOfBirthAttribute($value)
+    {
+    $this->attributes['date_of_birth'] = Carbon::createFromFormat('m/d/Y', $value)->format('Y-m-d');
+    }
+
+    public function getDateOfBirthAttribute($value)
+    {
+    return Carbon::createFromFormat('Y-m-d', $this->attributes['date_of_birth'])->format('m/d/Y');
+    }
+
+    public function getAgeAttribute() {
+    return Carbon::parse($this->attributes['date_of_birth'])->age;
+    }
+}
